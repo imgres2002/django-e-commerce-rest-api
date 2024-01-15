42df6d45382a8c16c70bca99d23fdc9745a2195a
imgres2002 <szymon.bieniaszewski2000@gmail.com>
2024-01-15 02:23:29 +0100

    Update matching products url

--- app/product/urls.py
+++ app/product/urls.py
@@ -7,6 +7,7 @@ app_name = 'products'
 
 urlpatterns = [
     path('product-list/', views.ProductListView.as_view(), name='product-list'),
+    path('matching-product-list/<int:pk>/', views.MatchingProductsListView.as_view(), name='matching-product-list'),
 ]
 
 router = SimpleRouter()
@@ -14,7 +15,6 @@ router = SimpleRouter()
 router.register('categories', views.CategoryViewSet, basename='categories')
 router.register('products', views.ProductViewSet, basename='products')
 router.register('opinions', views.OpinionViewSet, basename='opinions')
-router.register('matching-products-list', views.MatchingProductsViewSet, basename='matching-products-list')
 
 urlpatterns += router.urls
 
